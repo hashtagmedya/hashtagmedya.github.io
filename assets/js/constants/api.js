96ba4b0a1cae0e5bdac3afe3e91e7b58ce33fec0
Musa <musa.kundakci@manigo.com>
2024-04-10 15:26:17 +0300

    fix: pause on mouse enter slide

---
+++ assets/js/constants/api.js
@@ -0,0 +1,3 @@
+
+const createURL = (url) => `https://hashtagstrapi-2f857d5493de.herokuapp.com${url}`;
+const createAPI = (url) => createURL(`/api/${url}`);
---
+++ assets/js/home/injectors.js
@@ -0,0 +1,27 @@
+const injectEmployees = (employees) => employees.map(({name, field, image, links}, index) => $("#team-section-list").append(`
+<div class="swiper-slide" data-hash=${index}>
+<div class="wow fadeInUp"  data-wow-delay="400ms" data-wow-duration="1200ms">
+        <div class="bi-team-item-2 text-center">
+            <div class="bi-team-img-shape position-relative">
+                <span class="team-shape shape_1 position-absolute"><img src="assets/img/icon/tm-shape3.png" alt=""></span>
+                <span class="team-shape shape_2 position-absolute"><img src="assets/img/icon/tm-shape6.png" alt=""></span>
+                <div class="bi-team-img">
+                    <img src=${createURL(image.data.attributes.url)}></img>
+                </div>
+            </div>
+            <div class="bi-team-text headline">
+                <h3><a href="team-single.html">${name}</a></h3>
+                <span>${field}</span>
+            </div>
+            <div class="bi-team-social ul-li">
+                ${links ? `<ul>
+                    ${links.instagram ? `<li><a href="${links.instagram}" target="_blank"> <i class="fab fa-instagram"/></a></li>` : ''}
+                    ${links.linkedin ? `<li><a href="${links.linkedin}" target="_blank"> <i class="fab fa-linkedin-in"/></a></li>` : ''}
+                    ${links.Facebook ? `<li><a href="${links.Facebook}" target="_blank"> <i class="fab fa-facebook"/></a></li>` : ''}
+                    ${links.youtube ? `<li><a href="${links.youtube}" target="_blank"> <i class="fab fa-youtube"/></a></li>` : ''}
+                </ul>` : ''}
+            </div>
+        </div>
+    </div>
+    </div>
+    `))

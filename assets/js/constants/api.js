f967596831a9a9619529d4749441863ecc90f9e5
Musa <musa.kundakci@manigo.com>
2024-04-13 16:00:21 +0300

    feat: portfolio skeletons and prepared for api

--- assets/js/constants/api.js
+++ assets/js/constants/api.js
@@ -1,3 +1,5 @@
+const localURL = "http://localhost:1337";
+const remoteURL = "https://hashtagstrapi-2f857d5493de.herokuapp.com";
 
-const createURL = (url) => `https://hashtagstrapi-2f857d5493de.herokuapp.com${url}`;
+const createURL = (url) => `${remoteURL}${url}`;
 const createAPI = (url) => createURL(`/api/${url}`);

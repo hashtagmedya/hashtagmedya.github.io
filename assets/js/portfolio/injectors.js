7882c2087f01bfd28491d5d00a48f84a2eeb0c69
Musa <musa.kundakci@manigo.com>
2024-04-14 01:55:26 +0300

    feat: portfolio page search params and subcategory

---
+++ assets/js/portfolio/injectors.js
@@ -0,0 +1,71 @@
+const injectProjects = (projects) => {
+  projects.forEach((project) => {
+    const {
+      attributes: {
+        title: projectTitle,
+        subtitle: projectSubtitle,
+        image: {
+          data: {
+            attributes: { url: projectImageURL },
+          },
+        },
+        project_subcategory: {
+          data: {
+            id: categoryId,
+            attributes: { text },
+          },
+        },
+      },
+    } = project;
+    const element = document.createElement("div");
+    element.classList.add("bi-portfolio-item-3");
+    element.classList.add("filtr-item");
+    element.setAttribute("data-category", categoryId);
+    element.setAttribute("data-sort", "Busy streets"); //TODO: Change this
+    element.innerHTML = `
+              <div class="bi-portfolio-img-text-3 position-relative">
+                <div class="portfolio-img">
+                  <img src="${createURL(projectImageURL)}" alt="" />
+                </div>
+                <a
+                  class="porfolio-icon position-absolute d-flex align-items-center justify-content-center"
+                  href="#"
+                >
+                  <i class="fal fa-search"></i
+                ></a>
+                <div class="portfolio-text headline position-absolute">
+                  <span><a href="#">${projectTitle}</a></span>
+                  <h3><a href="#">${projectSubtitle}</a></h3>
+                  <a
+                    class="more_btn position-absolute d-flex align-items-center justify-content-center"
+                    href="#"
+                    ><i class="fal fa-long-arrow-right"></i
+                  ></a>
+                </div>
+              </div>
+            `;
+    element.addEventListener("click", () => {
+      window.location.href = `project.html?id=${project.id}`;
+    });
+    $("#bi-portfolio-feed-item-wrapper").append(element);
+  });
+};
+
+const injectCategories = (categories) => {
+  categories.forEach((category) => {
+    const listItem = document.createElement("div");
+    listItem.setAttribute("data-filter", category?.id);
+    listItem.classList.add("filtr-button");
+    listItem.innerText = category?.attributes?.text;
+    listItem.addEventListener("click", () => {
+      window.history.pushState
+        ? window.history.pushState(
+            null,
+            null,
+            `?category=${category.id}&categoryName=${category.attributes.text}`
+          )
+        : (window.location.hash = `?category=${category.id}&categoryName=${category.attributes.text}`);
+    });
+    $("#filters").append(listItem);
+  });
+};
